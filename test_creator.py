e403381923efe4c18f2be5b0816b3108c3ee7507
JMAndrades <yusei625@gmail.com>
2022-08-05 16:23:32 +0200

    v2

--- test_creator.py
+++ test_creator.py
@@ -1,30 +1,58 @@
-import sys
-
-file_name = sys.argv[1]
-# file_name = r'D:\proyectos\test_creator\1.sql'
+import sys, re
+# file_name = sys.argv[1]
+file_name = r'1.sql'
 file = open(file_name, 'r')
-file_content = file.read()
-file_content_list = file_content.split('\n')
-file_content = file_content.replace('\t', '').replace('\n', '')
-example_data = "("
-for x in range(5):
-    for row in file_content_list:
-        if 'CREATE' in row:
+file_content = file.readlines()
+example_data = "INSERT INTO "
+cont1 = 0
+for line in file_content:
+    if line == ')' or line == ");" or 'CONSTRAINT' in line.upper() or 'IDENTITY' in line.upper():
             pass
-        elif 'int' in row:
-            # get first word of the string 
-            example_data += row.split(' ')[0] + ' 1' + ", "
-        elif  'float' in row or 'double' in row or 'decimal' in row:
-            example_data += row.split(' ')[0] + ' 1.1' + ", "
-        elif 'varchar' in row:
-            example_data += row.split(' ')[0] + " 'test1'" + ", "
-        elif 'date' in row or 'datetime' in row or 'timestamp' in row:
-            example_data += row.split(' ')[0] + " 01-01-2022" + ", "
-        elif 'char' in row:
-            example_data += row.split(' ')[0] + " 't'" + ", "
-    if x < 4:
-        example_data = example_data[:-2] + "),\n("
+    elif 'CREATE' in line:
+        table_name = re.sub("\(","",line.split(" ")[2])
+        example_data += f"{table_name} ("
     else:
-        example_data = example_data[:-2] + ");"
-
+        nombre = re.sub("\\\\t","",line.split(" ")[0]).strip()
+        example_data += f"{nombre}, "
+        cont1 += 1
+example_data = example_data[:-2] + ") VALUES\n"
+for x in range(1,5):
+    cont2 = 0
+    example_data += "("
+    for line in file_content:
+        if 'CONSTRAINT' in line.upper():
+            pass
+        if 'IDENTITY' in line.upper():
+            pass
+        elif ' int ' in line or ' bigint ' in line or ' smallint ' in line or ' tinyint ' in line:
+            example_data += f"{x}, "
+            cont2 += 1
+        elif ' date ' in line or ' datetime ' in line or ' date2 ' in line or ' datetime2' in line or ' time ' in line or ' time2 ' in line or ' datetimeoffset ' in line:
+            example_data += f"'2022-08-0{x}', "
+            cont2 += 1
+        elif ' decimal(' in line or ' float(' in line:
+            example_data += f"{x}.{x-1}, "
+            cont2 += 1
+        elif ' varchar ' in line or ' nvarchar ' in line or ' char' in line or ' nchar' in line or ' varbinary' in line or ' binary' in line:
+            # longitud = int(re.sub("\).*$","",(re.sub(".*?\(","",line))))
+            longitud = re.sub("\).*$","",(re.sub(".*?\(","",line)))
+            print(longitud)
+            test = "test"[:longitud-1]
+            example_data += f"'{test}{x}', "
+            cont2 += 1
+        elif ' bit ' in line:
+            if x % 2 == 0:
+                example_data += f"1, "
+            else:
+                example_data += f"0, "
+            cont2 += 1
+    if x < 4:
+        example_data = example_data[:-2] +"),\n"
+    else:
+        example_data = example_data[:-2] +");"
+example_data = re.sub("\[|\]","",example_data)
 print(example_data)
+if cont1 != cont2:
+    print("There is something wrong!")
+    print(f"{cont1} rows read")
+    print(f"{cont2} rows written")
